17c394cc289eef304db05dcc4024e42a1f663700
Gautam Jain <astrogeek77@gmail.com>
2023-09-12 19:28:47 +0530

    style fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { useMemo } from "react"
-import { Container } from "react-bootstrap"
-import { Routes, Route, Navigate } from "react-router-dom"
-import { NewNote } from "./Pages/NewNote"
-import { useLocalStorage } from "./utils/useLocalStorage"
-import { v4 as uuidV4 } from "uuid"
-import { NoteList } from "./Pages/Home/Home"
-import { NoteLayout } from "./Layouts/NoteLayout"
-import { Note } from "./components/Note/Note"
-import { EditNote } from "./Pages/EditNote"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo } from 'react'
+import { Container } from 'react-bootstrap'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { NewNote } from './Pages/NewNote'
+import { useLocalStorage } from './utils/useLocalStorage'
+import { v4 as uuidV4 } from 'uuid'
+import { NoteList } from './Pages/Home/Home'
+import { NoteLayout } from './Layouts/NoteLayout'
+import { Note } from './components/Note/Note'
+import { EditNote } from './Pages/EditNote'
 
-// custum types 
+// custum types
 // Raw note types to store tag ids for easy updation
 export type Note = {
   id: string
@@ -40,32 +40,35 @@ export type Tag = {
 
 function App() {
   // save state locally(defined types)
-  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
-  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
 
   // using use memo to only run the code when notes value change.
   const notesWithTags = useMemo(() => {
-    return notes.map(note => {
-      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      }
     })
   }, [notes, tags])
 
   // utility for creating a note
   function onCreateNote({ tags, ...data }: NoteData) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [
         ...prevNotes,
-        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
       ]
     })
   }
 
   // utility for updating a note, by mapping over every note and matching with id
   function onUpdateNote(id: string, { tags, ...data }: NoteData) {
-    setNotes(prevNotes => {
-      return prevNotes.map(note => {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
         if (note.id === id) {
-          return { ...note, ...data, tagIds: tags.map(tag => tag.id) }
+          return { ...note, ...data, tagIds: tags.map((tag) => tag.id) }
         } else {
           return note
         }
@@ -75,20 +78,20 @@ function App() {
 
   // utility for delete a note
   function onDeleteNote(id: string) {
-    setNotes(prevNotes => {
-      return prevNotes.filter(note => note.id !== id)
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id)
     })
   }
 
   // utility to update the state with addition of a new tag
   function addTag(tag: Tag) {
-    setTags(prev => [...prev, tag])
+    setTags((prev) => [...prev, tag])
   }
 
   // utilty to run when a tag value is updated and update label by matching id
   function updateTag(id: string, label: string) {
-    setTags(prevTags => {
-      return prevTags.map(tag => {
+    setTags((prevTags) => {
+      return prevTags.map((tag) => {
         if (tag.id === id) {
           return { ...tag, label }
         } else {
@@ -100,21 +103,21 @@ function App() {
 
   // utility to run when a tag is deleted
   function deleteTag(id: string) {
-      // remove tag from state
-      setTags(prevTags => {
-        return prevTags.filter(tag => tag.id !== id)
-      })
+    // remove tag from state
+    setTags((prevTags) => {
+      return prevTags.filter((tag) => tag.id !== id)
+    })
 
-      /// update notes with removing deleted tag
-      setNotes(prevNotes => {
-      return prevNotes.map(note => {
-        return {...note, tagIds: note.tagIds.filter(tagId => tagId !== id)}
+    /// update notes with removing deleted tag
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        return { ...note, tagIds: note.tagIds.filter((tagId) => tagId !== id) }
       })
     })
   }
 
   return (
-    <Container className="my-4">
+    <Container className="my-4 p-4 bg-light bg-gradient">
       <Routes>
         <Route
           path="/"
